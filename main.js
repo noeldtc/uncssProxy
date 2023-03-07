7cfea8cc0799584eafddf0dbec74a6bee8fcdfd0
ddeltejo <daniel-noel.del-tejo-catala.external@capgemini.com>
2023-03-07 11:30:19 +0100

    Updated

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 
 $( document ).ready(function() {
     $.get("https://www.kimovil.com/es/comparar-moviles/f_max_d+eurPrice.450,f_not_m+code.CN:IN#localprices", function (response){ 
-        var html = response;
-        var html_src = 'data:text/html;charset=utf-8,' + html;
-        $("#iframeId").attr("src" , html_src);
+        document.getElementsByTagName("body")[0].innerHTML = response;
     });
 });
 
